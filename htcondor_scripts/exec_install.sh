0372344ae58869265cbedf7f5a5bbafe24365828
Mareike <mareikeh@lst.uni-saarland.de>
2026-05-06 12:38:32 +0200

    updated requirements

--- htcondor_scripts/exec_install.sh
+++ htcondor_scripts/exec_install.sh
@@ -4,6 +4,6 @@
 source /scratch/hartmann/miniconda3/etc/profile.d/conda.sh
 echo "Current conda environment: $CONDA_DEFAULT_ENV"
 
-conda create --name lst_agent_example
+conda create --name lst_agent_example python=3.12
 conda activate lst_agent_example
 pip install -r /nethome/mareikeh/lst_agent_example/requirements.txt
