f70512fdfc5793adf4034c37d9e52a18dbe07da7
mahartmann <mrkhartmann4@gmail.com>
2026-05-07 00:25:06 +0200

    Update pip install path to use relative requirements file

--- htcondor_scripts/exec_install.sh
+++ htcondor_scripts/exec_install.sh
@@ -6,4 +6,4 @@ echo "Current conda environment: $CONDA_DEFAULT_ENV"
 
 conda create --name lst_agent_example python=3.12
 conda activate lst_agent_example
-pip install -r /nethome/mareikeh/lst_agent_example/requirements.txt
+pip install -r requirements.txt
